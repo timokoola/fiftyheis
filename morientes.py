59c2ed2b4071b579f13fda7b6cbef2b8cfae4bf9
Timo Koola <timo.koola@codento.com>
2015-03-12 22:21:46 +0200

    valmista taisi tulla

--- morientes.py
+++ morientes.py
@@ -5,11 +5,9 @@ from twython import Twython
 from twython.exceptions import TwythonError
 import re
 import pprint
-import sqlite3
 import time
 import json
 import random
-conn = sqlite3.connect('moitweets.db')
 r = random.Random()
 
 heis = ["moi", "moikka", "mitäs", "terve", "terppa", "morienttes", "mitämitä", "tsaukki", "mitä äijä", "heippa", "hellurei", "hei sun heiluvilles", "moro", "poro", "päivää", "päevää", "morjens", "tervis", "terskamaheppi", "termos", "heissan", "morjestarallaa", "terve mieheen", "moikku", "hola", "heissuli", "jassoo", "hejdå", "vieläk sääki elät", "helou", "tervehdys", "terve vaan terve", "tsau", "kuisso", "moikkandeeros", "heip", "moikkanen", "komiaa", "moro nääs", "muija", "tähän käteen", "terveisii", "ehtoota talohon", "hai sie", "huomentapäivää", "tseenare", "aloha", "tere", "jumalan terve", "heipparallaa"]
@@ -20,12 +18,6 @@ api = None
 ats = re.compile("@\w+")
 url = re.compile("http://\S+")
 risu = re.compile("#\S+")
-
-def save_tweet_db(tweet):
-    c = conn.cursor()
-    row = ("%f" % time.time(), json.dumps(tweet), tweet["user"]["id"], tweet["text"])
-    c.execute('INSERT INTO tweets VALUES (?,?,?,?)', row)
-    conn.commit()
 
 
 class TwythonHelper:
@@ -51,11 +43,8 @@ class MyStreamer(TwythonStreamer):
             if data["user"]["screen_name"] == me:
                 print "My own tweet"
                 return
-            save_tweet_db(data)
-            if data["lang"] != "fi":
-                print "Ei suomea"
-                return
-            status = r.choice(heis).title() + "!"
+            text = r.choice(heis) 
+            status = text[0].upper() + text[1:] + "!"
             try:
                 api.update_status(status=status, in_reply_to_status_id=data["id"])
             except TwythonError:
